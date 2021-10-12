fbc31658f14d48f808f23bcda18065effd39c2dd
VerbindingMaken <74724974+VerbindingMaken@users.noreply.github.com>
2021-10-12 12:49:37 +0200

    Added content for "start-maken" made responsive

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // CHANGE COLOR slider
-
+/*
 // GET ROOT ELEMENT
 const r = document.querySelector(':root')
 
-/* HTML:
+HTML:
 <li><label for="color">Pas kleur aan:</label></li>
 <li><input id="color" name="color" type="range" min="0" max="360" value="0"></li>
-*/
+
 
 const colorSlider = document.querySelector('#color');
 
@@ -16,13 +16,5 @@ function changeColor() {
 
 colorSlider.addEventListener('change', changeColor);
 colorSlider.addEventListener('mousemove', changeColor);
-// SHOW OFFER button
-const showOfferButton = document.querySelector('#show-offer');
-const secondParagraphs = document.querySelectorAll('.second');
 
-function showOffer() {
-    showOfferButton.style.display = "none";
-    secondParagraphs.forEach(paragraph => paragraph.style.display = "block")
-}
-
-showOfferButton.addEventListener('click', showOffer);
+*/
